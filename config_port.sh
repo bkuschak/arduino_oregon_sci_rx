3181b004b429c55fe0f60b5a5187689b36c09b1f
root <root@beaglebone.localdomain>
2019-02-25 03:55:09 +0000

    Use full path since we call this from /etc/rc.local

--- config_port.sh
+++ config_port.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Enable UART4 if necessary
 
-sudo config-pin P9.11 uart
-sudo config-pin -q P9.11
-sudo config-pin P9.13 uart
-sudo config-pin -q P9.13
+/usr/local/bin/config-pin P9.11 uart
+/usr/local/bin/config-pin -q P9.11
+/usr/local/bin/config-pin P9.13 uart
+/usr/local/bin/config-pin -q P9.13
